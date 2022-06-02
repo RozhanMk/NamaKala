3b37d28cf445f522a462b7af0aacc8eb797b213d
RozhanMk <rozhan.mirzaei82@gmail.com>
2022-06-02 17:07:33 +0430

    added sorting page

--- lib/models/group.dart
+++ lib/models/group.dart
@@ -10,4 +10,27 @@ class Group {
   });
 }
 
-List<Group> groups = [];
+List<Group> groupDigital = [
+  Group(name: "موبایل", image: "assets/images/mobile.png"),
+  Group(name: "لپتاپ", image: "assets/images/laptop.png"),
+  Group(name: "دوربین", image: "assets/images/camera.png"),
+];
+
+List<Group> groupClothes = [
+  Group(name: "زنانه", image: "assets/images/women.png"),
+  Group(name: "مردانه", image: "assets/images/men.png"),
+  Group(name: "بچگانه", image: "assets/images/baby.png"),
+];
+
+List<Group> groupBook = [
+  Group(name: "کتاب", image: "assets/images/book.png"),
+  Group(name: "لوازم التحریر", image: "assets/images/stationery.png"),
+  Group(name: "موسیقی", image: "assets/images/music.png"),
+  Group(name: "صنایع دستی", image: "assets/images/handicrafts.png"),
+];
+
+List<Group> groupSport = [
+  Group(name: "پوشاک ورزشی", image: "assets/images/sport cloth.png"),
+  Group(name: "لوازم ورزشی", image: "assets/images/sport equipment.png"),
+  Group(name: "لوازم سفر و کمپینگ", image: "assets/images/camping.png"),
+];
--- lib/pages/Sorting/sortScreen.dart
+++ lib/pages/Sorting/sortScreen.dart
@@ -1,5 +1,5 @@
 import 'package:example/const.dart';
-import 'package:example/sizeHandler.dart';
+import 'package:example/models/group.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
@@ -8,38 +8,206 @@ class sortingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Directionality(
-        textDirection: TextDirection.rtl,
-        child: Stack(
-          children: [
-            
-            Container(
-            margin: EdgeInsets.symmetric(vertical: 50 , horizontal: 15),
-            //width:  MediaQuery.of(context).size.width * 0.8,
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 212, 196, 218).withOpacity(0.5),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: TextField(
-              onChanged: (value) => print(value),
-              decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 9),
-                  border: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  hintText: "جستجو در نماکالا",
-                  hintStyle: TextStyle(
-                    fontFamily: "A Mitra 05",
-                    fontSize: 15,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: SingleChildScrollView(
+            child: Stack(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 50, horizontal: 15),
+                  //width:  MediaQuery.of(context).size.width * 0.8,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 212, 196, 218).withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                  prefixIcon: Icon(Icons.search),iconColor: Color.fromRGBO(110, 10, 138, 1)),
+                  child: TextField(
+                    onChanged: (value) => print(value),
+                    decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 9),
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        hintText: "جستجو در نماکالا",
+                        hintStyle: TextStyle(
+                          fontFamily: "A Mitra 05",
+                          fontSize: 15,
+                        ),
+                        prefixIcon: Icon(Icons.search),
+                        iconColor: Color.fromRGBO(110, 10, 138, 1)),
+                  ),
+                ),
+                Column(
+                  children: [
+                    SizedBox(height: 110),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 20),
+                      child: Text(
+                        "کالای دیجیتال",
+                        style: TextStyle(
+                          fontFamily: "A Mitra 05",
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                  ),  
+                ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    SizedBox(height: 140),
+                    Container(
+                      height: 150,
+                      child: ListView.separated(
+                        padding: EdgeInsets.all(5),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 3,
+                        separatorBuilder: (context, _) => SizedBox(width: 10),
+                        itemBuilder: (context, index) =>
+                            buildCard(groupDigital.elementAt(index)),
+                      ),
+                    ),
+                  ],
+                ),
+          
+                Column(
+                  children: [
+                    SizedBox(height: 300),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 20),
+                      child: Text(
+                        "پوشاک",
+                        style: TextStyle(
+                          fontFamily: "A Mitra 05",
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                  ),  
+                ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    SizedBox(height: 330),
+                    Container(
+                      height: 150,
+                      child: ListView.separated(
+                        padding: EdgeInsets.all(5),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 3,
+                        separatorBuilder: (context, _) => SizedBox(width: 10),
+                        itemBuilder: (context, index) =>
+                            buildCard(groupClothes.elementAt(index)),
+                      ),
+                    ),
+                  ],
+                ),
+          
+                Column(
+                  children: [
+                    SizedBox(height: 480),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 20),
+                      child: Text(
+                        "کتاب و لوازم التحریر",
+                        style: TextStyle(
+                          fontFamily: "A Mitra 05",
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                  ),  
+                ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    SizedBox(height: 520),
+                    Container(
+                      height: 150,
+                      child: ListView.separated(
+                        padding: EdgeInsets.all(5),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 4,
+                        separatorBuilder: (context, _) => SizedBox(width: 10),
+                        itemBuilder: (context, index) =>
+                            buildCard(groupBook.elementAt(index)),
+                      ),
+                    ),
+                  ],
+                ),
+          
+                Column(
+                  children: [
+                    SizedBox(height: 680),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 20),
+                      child: Text(
+                        "ورزش و سفر",
+                        style: TextStyle(
+                          fontFamily: "A Mitra 05",
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                  ),  
+                ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    SizedBox(height: 720),
+                    Container(
+                      height: 150,
+                      child: ListView.separated(
+                        padding: EdgeInsets.all(5),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 3,
+                        separatorBuilder: (context, _) => SizedBox(width: 10),
+                        itemBuilder: (context, index) =>
+                            buildCard(groupSport.elementAt(index)),
+                      ),
+                    ),
+                  ],
+                ),
+                
+              ],
             ),
-              ),
-          ],
           ),
+        ),
       ),
     );
   }
+
+  Widget buildCard(Group group) => SizedBox(
+    width: 140,
+    child: Column(
+      children: [
+        Expanded(
+          child: AspectRatio(
+            aspectRatio: 1.02,
+            child: InkWell(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 212, 196, 218).withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Image.asset(group.image),
+              ),
+              onTap: () {},
+            ),
+          ),
+        ),
+        Text(
+          group.name,
+          style: TextStyle(
+            fontFamily: "A Mitra 05",
+            fontSize: 12,
+          ),
+        ),
+      ],
+    ),
+  );
 }
